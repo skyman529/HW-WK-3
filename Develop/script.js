3ab36efa507bddb279df257a4d5c4350a7234334
skyman529 <skyman529@gmail.com>
2022-09-07 19:39:31 -0600

    updated code for resubmital

--- Develop/script.js
+++ Develop/script.js
@@ -1,59 +1,59 @@
-// The Password generator will provide a password with 8-128  the user specifies length and complexity.
+// Skylar's Password generator will provide passwords with lengths from 8 charcters to 128 the user specifies length and complexity.
 
-// event listener to prompt question when button pushed
+// event listner for when is button pushed
 document.querySelector("#generate").addEventListener("click", writePassword);
 
 //  Arrays of characters 
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var numerals = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 var superspecialChar = ["$", "$", "$", "$", "$", "$", "$", "$", "$", "$", "$", "$","$"];
-var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-if (condition) {
-  
-} else {
-  
-}
-// Variable Declaration 
+var letterLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var letterUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// declare variables
 var confirmLength = "";
-var confirmSpecialCharacter;
-var confirmSuperSpecialCharacter;
-var confirmNumericCharacter;
 var confirmUpperCase;
 var confirmLowerCase;
+var confirmSpecCharacter;
+var confirmSuperSpecialCharacter;
+var confirmNumericCharacter;
 
-// Prompt to confirm how many characters the user would like in their password
+
+ // verify user is crazy 
+ alert(`You're using a password generator created by a fourth week DU coding bootcamp student. apply passwords at your own risk`);
+// prompt for length
 function generatePassword() {
-  var confirmLength = (prompt("How many characters would you like your password to contain? any 8 to 128."));
+  var confirmLength = (prompt("How many characters would you like your password to contain? any 8 to 128. I would use as many charecters as you're allowed."));
 
-  // Loop if answer is outside the parameters 
+  // Loop if answer to small or large
   while(confirmLength <= 7 || confirmLength >= 129) {
-      alert("Password length must be between 8-128 characters Try again");
+      alert("Password length must be between 8-128 characters please try again");
       var confirmLength = (prompt("How many characters would you like your password to contain?"));
       } 
 
-      // Repeat back how many charactes the user will have  
-      alert(`Your password will have ${confirmLength} characters`);
+      // verify legth 
+      alert(`You've chosen ${confirmLength} characters!`);
 
-    // Determine parameters of password 
-    var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+    // password options verification 
+    var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+    var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters"); 
+    var confirmSpecCharacter = confirm("Click OK to confirm if you would like to include special characters");
     var confirmSuperSpecialCharacter = confirm("Click OK to confirm if you would like to include Super special characters");
     var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
-    var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
-    var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
-      // Loop if answer is outside the parameters 
+      // Loop if  has no options selected
      
-      while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmSuperSpecialCharacter === false && confirmNumericCharacter === false) {
+      while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecCharacter === false && confirmSuperSpecialCharacter === false && confirmNumericCharacter === false) {
         alert("You must choose at least one parameter");
-        var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+        var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+        var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters"); 
+        var confirmSpecCharacter = confirm("Click OK to confirm if you would like to include special characters");
         var confirmSuperSpecialCharacter = confirm("Click OK to confirm if you would like to include Super special characters");
         var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
-        var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
-        var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");   
     } 
-    while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmSuperSpecialCharacter === true && confirmNumericCharacter === false) {
+    // Loop if only superSpecialchar is selected
+    while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecCharacter === false && confirmSuperSpecialCharacter === true && confirmNumericCharacter === false) {
       alert("That is very cash money of you but, you should choose at least one more parameter");
-      var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+      var confirmSpecCharacter = confirm("Click OK to confirm if you would like to include special characters");
       var confirmSuperSpecialCharacter = confirm("Click OK to confirm if you would like to include Super special characters");
       var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
       var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
@@ -64,8 +64,8 @@ function generatePassword() {
       // Assign an action to the password parameters
       var passwordCharacters = []
       
-    if (confirmSpecialCharacter) {
-      passwordCharacters = passwordCharacters.concat(specialChar)
+    if (confirmSpecCharacter) {
+      passwordCharacters = passwordCharacters.concat(specChar)
     }
 
     if (confirmSuperSpecialCharacter) {
@@ -73,15 +73,15 @@ function generatePassword() {
     }
 
     if (confirmNumericCharacter) {
-      passwordCharacters = passwordCharacters.concat(number)
+      passwordCharacters = passwordCharacters.concat(numerals)
     }
       
     if (confirmLowerCase) {
-      passwordCharacters = passwordCharacters.concat(alphaLower)
+      passwordCharacters = passwordCharacters.concat(letterLower)
     }
 
     if (confirmUpperCase) {
-      passwordCharacters = passwordCharacters.concat(alphaUpper)
+      passwordCharacters = passwordCharacters.concat(letterUpper)
     }
 
       console.log(passwordCharacters)
@@ -96,7 +96,7 @@ function generatePassword() {
       return randomPassword;
 }
 
-// Write password to the #password input
+// show password in #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
